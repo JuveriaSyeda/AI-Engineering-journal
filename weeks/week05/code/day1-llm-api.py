10042e7fd7b289df0d56f5ec699d8479f0cd63c8
Zuberia-Syed <zuberia01@gmail.com>
2026-05-03 21:10:10 +0530

    calling llm-apis

--- weeks/week05/code/day1-llm-api.py
+++ weeks/week05/code/day1-llm-api.py
@@ -1,16 +1,28 @@
 from dotenv import load_dotenv
 import os
 from google import genai
+from openai import OpenAI
+
 
 load_dotenv()
 
 apiKey = os.getenv("GEMINI_API_KEY")
+groqApiKey = os.getenv("GROQ_API_KEY")
+# client = genai.Client(api_key=apiKey)
 
-client = genai.Client(api_key=apiKey)
+# response = client.models.generate_content(
+#    model="gemini-3-flash-preview",
+#     contents="Explain about RAG in simple words",
+# )
 
-response = client.models.generate_content(
-   model="gemini-3-flash-preview",
-    contents="Explain about RAG in simple words",
+# print(response.text)
+
+client = OpenAI(
+    api_key=groqApiKey,
+    base_url="https://api.groq.com/openai/v1"
 )
-
-print(response.text)
+response = client.responses.create(
+    input="Explain about LLMs in simple words",
+    model="openai/gpt-oss-20b"
+)
+print(response.output_text)
